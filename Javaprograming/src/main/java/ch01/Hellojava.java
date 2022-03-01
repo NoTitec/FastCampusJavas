8afd0443fa8cf1a1665d427a1cf7327975890f5a
NoTitec <zkxkr0@gmail.com>
2022-03-01 16:08:03 +0900

    Initial commit

---
+++ Javaprograming/src/main/java/ch01/Hellojava.java
@@ -0,0 +1,8 @@
+package ch01;
+
+public class Hellojava {
+    public static void main(String[] args) {//내 로컬컴퓨터 웹서비구동인경우 main아님
+        System.out.println("Hello java");
+        System.out.println("");
+    }
+}
---
+++ Javaprograming/src/main/java/ch04/Binarytest.java
@@ -0,0 +1,16 @@
+package ch04;
+
+public class Binarytest {
+
+    public static void main(String[] args) {
+        int num=10;
+        int bnum=0B1010;
+        int onum=012;
+        int xnum=0xA;
+
+        System.out.println(num);
+        System.out.println(bnum);
+        System.out.println(onum);
+        System.out.println(xnum);
+    }
+}
