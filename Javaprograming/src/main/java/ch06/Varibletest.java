8e967268fd69d9c502ef455a6e0908b4d3a7ce2a
NoTitec <zkxkr0@gmail.com>
2022-03-01 16:42:21 +0900

    ch6s

---
+++ Javaprograming/src/main/java/ch06/Varibletest.java
@@ -0,0 +1,13 @@
+package ch06;
+
+public class Varibletest {
+    public static void main(String[] args) {
+        byte bnum = 127;
+
+        System.out.println(bnum);
+
+        long num = 12345678900L;//int범위내의 수는 L없이넣어도 가능
+
+        System.out.println(num);
+    }
+}
